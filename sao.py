6c66c88310aa105aa17f002464bcbf7d2a5e907d
Christian Janson <christianjanson99@gmail.com>
2026-03-31 21:04:45 +0700

    first atm project

---
+++ sao.py
@@ -0,0 +1,48 @@
+print("welcome to nazria")
+print("please choose your gender")
+
+while True:
+    print("\njenis kelamin: ")
+    print("1. man")
+    print("2. woman")
+    pilih = str(input("choose your gender: "))
+
+    if pilih == "1":
+        print("choose your defense: ")
+        while True:
+            print("\nkarakter: ")
+            print("1. Athena")
+            print("2. Arthemis")
+            print("3. Maria")
+            print("4. Hades")
+            print("5. Neptunus")
+            print("6. Triton")
+            print("7. Apollo")
+            print("8. Hera")
+            choose1 = str(input("please choose your defense"))
+            print("your defense is" + choose1)
+            print("sayonara, enjoy your game")
+
+    elif pilih == "2":
+        print("choose your defense: ")
+        while True:
+            print("\nkarakter: ")
+            print("1. Athena")
+            print("2. Arthemis")
+            print("3. Maria")
+            print("4. Hades")
+            print("5. Neptunus")
+            print("6. Triton")
+            print("7. Apollo")
+            print("8. Hera")
+            choose2 = str(input("please choose your defense"))
+            print("your defense is" + choose2)
+            print("sayonara, enjoy your game")
+
+    else:
+        print("please choose right choice")
+
+
+
+
+    
